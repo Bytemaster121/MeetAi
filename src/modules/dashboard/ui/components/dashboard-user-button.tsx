3b48faf601c975217fca93621be9d579798a9950
Tanmay saxena <saxenatanmay2022@gmail.com>
2025-08-17 22:44:53 +0530

    06 dashboard sidebar(02)

--- src/modules/dashboard/ui/components/dashboard-user-button.tsx
+++ src/modules/dashboard/ui/components/dashboard-user-button.tsx
@@ -9,14 +9,31 @@ import {
 } from "@/components/ui/dropdown-menu";
 import { Avatar , AvatarImage } from "@/components/ui/avatar";
 import { GeneratedAvatar } from "@/components/generated-avatar";
-import { ChevronDownIcon } from "lucide-react";
+import { ChevronDownIcon, CreditCardIcon, LogOutIcon } from "lucide-react";
+import { useRouter } from 'next/navigation';
+
 
 
 export const DashboardUserButton = () =>{
+    const router = useRouter();
     const { data, isPending } = authClient.useSession();
     if(isPending || !data ?.user)  {
         return null;
     }
+
+    const onLogout = () => {
+        
+             authClient.signOut({
+                fetchOptions: {
+                    onSuccess: ( ) =>{
+                        router.push("/sign-in");
+
+                    }
+                }
+            });
+        
+    }
+
     return (
         <DropdownMenu>
             <DropdownMenuTrigger className="rounded-full border border-border/10 p-3 w-full flex items-center justify-between bg-white/5 hover:bg-white/10 overflow-hidden">
@@ -40,9 +57,18 @@ export const DashboardUserButton = () =>{
             <DropdownMenuContent align="end">
                 <DropdownMenuLabel>{data.user.email}</DropdownMenuLabel>
                 <DropdownMenuSeparator />
-                <DropdownMenuItem>Profile</DropdownMenuItem>
-                <DropdownMenuItem>Settings</DropdownMenuItem>
-                <DropdownMenuItem>Logout</DropdownMenuItem>
+
+                <DropdownMenuItem
+                className="cursor-pointer flex items-center  justify-between">
+                    Billing 
+                    <CreditCardIcon  className="size-4"/>
+                </DropdownMenuItem>
+                <DropdownMenuItem
+                onClick={onLogout}
+                className="cursor-pointer flex items-center  justify-between">
+                    Logout
+                    <LogOutIcon className="size-4" />
+                </DropdownMenuItem>
             </DropdownMenuContent>
         </DropdownMenu>
     );
