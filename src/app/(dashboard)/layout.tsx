bb28dcf6d51dd13c2fbfd30683cef1488910b765
Tanmay saxena <saxenatanmay2022@gmail.com>
2025-08-18 23:00:02 +0530

    07: Dashboard nav bar

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,5 +1,6 @@
 import { SidebarProvider } from "@/components/ui/sidebar";
 import { DashboardSideBar } from "@/modules/dashboard/ui/components/Dashboard-SideBar";
+import { DashboardNavbar } from "@/modules/dashboard/ui/components/DashboardNavbar";
 interface Props {
     children: React.ReactNode;
 }
@@ -9,6 +10,7 @@ const Layout = ({ children }: Props) => {
     <SidebarProvider>
       <DashboardSideBar />
       <main className="flex flex-col h-screen w-screen bg-muted">
+        <DashboardNavbar />
         {children}
       </main>
     </SidebarProvider>
